0aab58fdb35066fc2bfee03b13a36522d5254d75
miyuu kuwana <miyuu13k@gmail.com>
2020-02-10 14:43:12 +0900

    first commit

---
+++ app/src/main/java/app/miyuseru/count2/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.miyuseru.count2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var number = 0
+
+    constructor() : super()
+//    var number :Int = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button1.setOnClickListener{
+
+            number = number +1
+            countText.text = number.toString()
+
+
+        }
+    }
+}
